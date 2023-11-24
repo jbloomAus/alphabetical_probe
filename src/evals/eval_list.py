ec18f3d556974d46eb4549fb47fe5782dcdc23b1
JayBaileyCS <sc2salivanth@gmail.com>
2023-11-24 14:28:05 +1000

    Got other evals mostly working, need to fix formatting

--- src/evals/eval_list.py
+++ src/evals/eval_list.py
@@ -1,14 +1,17 @@
 from evals.spelling_by_grade import *
 
 # Evals
-FULL_SPELLING_EVAL = SpellingEval(create_full_spelling_prompt, 
-                                  format_full_spelling_response, 
-                                  get_spelling_accuracy)
-GET_FIRST_LETTER_EVAL = SpellingEval(create_first_letter_spelling_prompt, 
-                                     format_full_spelling_response, 
-                                     get_spelling_accuracy)
-GET_POSITION_OF_LETTER_EVAL = SpellingEval(create_position_of_letter_spelling_prompt, 
-                                           format_position_of_letter_spelling_response, 
-                                           get_position_of_letter_accuracy)
+FULL_SPELLING_EVAL = GradeSpellingEval("Spell Full Word", 
+                                       create_full_spelling_prompt, 
+                                       format_full_spelling_response, 
+                                       get_spelling_accuracy)
+GET_FIRST_LETTER_EVAL = GradeSpellingEval("Spell First Letter", 
+                                          create_first_letter_spelling_prompt, 
+                                          format_first_letter_response, 
+                                          get_spelling_accuracy)
+GET_POSITION_OF_LETTER_EVAL = GradeSpellingEval("Get Position of Letter In Word", 
+                                                create_position_of_letter_spelling_prompt, 
+                                                format_position_of_letter_response, 
+                                                get_position_of_letter_accuracy)
 
 EVAL_LIST = [FULL_SPELLING_EVAL, GET_FIRST_LETTER_EVAL, GET_POSITION_OF_LETTER_EVAL]
--- src/evals/spelling_by_grade.py
+++ src/evals/spelling_by_grade.py
@@ -6,21 +6,24 @@ import re
 
 # TODO: Remove tokenizer, use model.tokenizer if possible.
 # TODO: Add "is letter in word" eval.
+# TODO: Genericise the prompt functions a bit more, there's repeated code here.
 # Should this be in eval_utils?
 class SpellingEvalDict(TypedDict):
+    """Defines the structure of a dictionary containing the data for a spelling eval prompt."""
     word: str
     prompt: str
     answer: str
     response: str
     
 
-class EvalResponseDict(TypedDict):
+class SpellingEvalResponseDict(TypedDict):
+    """Defines the structure of a dictionary containing the data and accuracy for a spelling eval."""
     data: Dict[int, List[SpellingEvalDict]]
     accuracy: Dict[int, float]
     
     
 # TODO: Consider an Eval generic class that can be extended for different evals.
-class SpellingEval:
+class GradeSpellingEval:
     """An object that contains the functions needed to run an eval on spelling by grade.
     Together, the functions define the evaluation.
     
@@ -30,7 +33,8 @@ class SpellingEval:
     format_response_function: A function that takes in the response from the model and formats it as needed.
     metric_function: A function that takes in a dictionary of {grade: [{prompt: str, answer: str, response: str}, ...], ...
     and judges the accuracy of the responses based on the answers."""
-    def __init__(self, prompt_function: callable, format_response_function: callable, metric_function: callable):
+    def __init__(self, name, prompt_function: callable, format_response_function: callable, metric_function: callable):
+        self.name = name
         self.prompt_function = prompt_function
         self.format_response_function = format_response_function
         self.metric_function = metric_function
@@ -50,33 +54,44 @@ class SpellingEval:
         return self.metric_function(data)
     
     def run_eval(self, model, model_type: ModelType, tokenizer, 
-                 word_list: List[Tuple[str, str]], 
-                 num_shots: int, batch_size: int=10) -> EvalResponseDict:
+                 word_list: Dict[int, List[Tuple[str, str]]], 
+                 num_shots: int, batch_size: int=10) -> SpellingEvalResponseDict:
         """Runs the eval on a given word list and number of shots.
         
         args:
         model: Contains the HuggingFace or TransformerLens model to run inference on.
         model_type: Determines what type of model we should use, which determines how we run inference.
         tokenizer: Contains the tokenizer to apply to prompts.
-        word_list: A list of tuples of the form (word, spelling) to run the eval on.
+        word_list: A list of tuples of the form (word, spelling) to run the eval on, separated by grade.
         num_shots: How many shots to give the model in evaluation.
         batch_size: How many prompts to pass in at once to the model.
         
         Returns:
-        An EvalResponse Dict containing the data and accuracy of the eval.
+        An EvalResponseDict containing the data and accuracy of the eval.
         """
         data = {grade: [] for grade in word_list.keys()}
     
         for grade in word_list:
             print(f"Assessing Grade {grade}")
-            prompts, answers = zip([self.prompt_function(word[0], word_list[grade], num_shots) for word in word_list[grade]])
+            prompts, answers = zip(*[self.prompt_function(word[0], word_list[grade], num_shots) for word in word_list[grade]])
             data[grade] = run_inference_on_model(model, model_type, tokenizer, prompts, answers, batch_size)
  
-        response: EvalResponseDict = {'data': data, 'accuracy': self.get_accuracy(data)}
+        response: SpellingEvalResponseDict = {'data': data, 'accuracy': self.get_accuracy(data)}
         return response
-        
     
-# Full spelling eval
+    def run_eval_with_multiple_shots(self, model, model_type: ModelType, tokenizer, 
+                                     word_list: Dict[int, List[Tuple[str, str]]], 
+                                     shots: List[int], batch_size: int=10) -> Dict[int, SpellingEvalResponseDict]:
+        """Runs the eval on a given word list for multiple few-shot parameters.
+        Same as run_eval but for multiple few-shot parameters.
+        
+        Returns: A dictionary of {num_shots: EvalResponseDict}."""
+        shots = {shot: {} for shot in shots}
+        for shot in shots:
+            shots[shot] = self.run_eval(model, model_type, tokenizer, word_list, shot, batch_size)
+        return shots    
+        
+        
 def prepare_grade_spelling_eval(filename: str, separator: str, case='upper'):
     """Takes a file with words separated by grades. 
     Returns a dictionary of tuples (word, spelling) by grade.
@@ -94,27 +109,28 @@ def prepare_grade_spelling_eval(filename: str, separator: str, case='upper'):
         for i, idx in enumerate(grade_indices):
             if i+1 < len(grade_indices): # Ensure we don't overflow.
                 for l in range(idx+2, grade_indices[i+1] - 1): # Collect all words of the right grade.
-                    words_by_grade[i+1].append((lines[l].strip(), get_spelling(lines[l].strip(), separator, case)))
+                    if lines[l].strip() != '':
+                        words_by_grade[i+1].append((lines[l].strip(), get_spelling(lines[l].strip(), separator, case)))
 
     return words_by_grade
 
-
-def create_full_spelling_prompt(word: str, word_list: Tuple[str, str], num_shots: int) -> Tuple[str, str]:
+# Full spelling eval
+def create_full_spelling_prompt(word: str, word_list: List[Tuple[str, str]], num_shots: int) -> Tuple[str, str]:
     """Takes in a word we want to spell, a list of words to sample from, and the number of shots.
-    Tuple is of the form (word, spelling).
+    Tuples are of the form (word, spelling).
     Creates a prompt that asks for the full spelling of a word.
     
     Returns the prompt and the answer to the prompt."""
     assert 0 <= num_shots < len(word_list), "Number of shots must be between 0 and the number of words in the list, minus the chosen word."
-    assert word in word_list, "Word must be in the word list."
+    _, answer = [w for w in word_list if w[0] == word][0] # Assumes unique words in word_list. TODO: Improve?
+    word_list = [item for item in word_list if item[0] != word] # Remove any words that are the same as the word we want to spell.
     prompt = ''
-    word_list.remove(word)
     if num_shots > 0:
         samples = random.sample(word_list, num_shots)
         for sample in samples:
             prompt += f"Q: How do you spell '{sample[0]}'? A: {sample[1]}\n\n"
     prompt += f"Q: How do you spell '{word}'? A:"
-    return prompt, word_list[word][1]
+    return prompt, answer
 
 
 def format_full_spelling_response(item):
@@ -122,23 +138,27 @@ def format_full_spelling_response(item):
     return item['response'].split('A: ')[-1].split("\n\n")[0].replace('\n', '').replace('<|endoftext|>', '').strip()
 
 
+def get_spelling_accuracy(data: Dict[int, List[SpellingEvalDict]]) -> Dict[int, float]:
+    """Takes in a dictionary of results, and returns the accuracy of the model on each grade."""
+    return {grade: sum([1 if d['answer'] in d['response'].upper() else 0 for d in data[grade]]) / len(data[grade]) for grade in data.keys()}
+
+
 # First letter eval
-def create_first_letter_spelling_prompt(word: str, word_list: Tuple[str, str], num_shots: int) -> Tuple[str, str]:
+def create_first_letter_spelling_prompt(word: str, word_list: List[Tuple[str, str]], num_shots: int) -> Tuple[str, str]:
     """Takes in a word we want to spell, a list of words to sample from, and the number of shots.
     Tuple is of the form (word, spelling).
     Creates a prompt that asks for the first letter of a word.
     
     Returns the prompt and the answer to the prompt."""
     assert 0 <= num_shots < len(word_list), "Number of shots must be between 0 and the number of words in the list, minus the chosen word."
+    word_list = [item for item in word_list if item[0] != word] # Remove any words that are the same as the word we want to spell.
     prompt = ''
-    if word in word_list:
-        word_list.remove(word) # Ensure we don't sample the same word we want to spell.
     if num_shots > 0:
         samples = random.sample(word_list, num_shots)
         for sample in samples:
-            prompt += f"Q: What is the first letter of '{sample[0]}'? A: {sample[1][0]}\n\n"
+            prompt += f"Q: What is the first letter of '{sample[0]}'? A: {sample[1][0].upper()}\n\n"
     prompt += f"Q: What is the first letter of '{word}'? A:"
-    return prompt, word[0]
+    return prompt, word[0].upper()
 
 
 def format_first_letter_response(item):
@@ -150,24 +170,23 @@ def format_first_letter_response(item):
 INDEX_TO_POSITION = {0: 'first', 1: 'second', 2: 'third', 3: 'fourth', 4: 'fifth', 5: 'sixth', 6: 'seventh', 
                      7: 'eighth', 8: 'ninth', 9: 'tenth', 10: 'eleventh', 11: 'twelfth'}
 
-def create_position_of_letter_spelling_prompt(word: str, word_list: Tuple[str, str], num_shots: int):
+def create_position_of_letter_spelling_prompt(word: str, word_list: List[Tuple[str, str]], num_shots: int):
     """Takes in a word we want to spell, a list of words to sample from, and the number of shots.
     Tuple is of the form (word, spelling).
     Creates a prompt that asks what position a letter of a word is in.
     
     Returns the prompt and the answer to the prompt."""
     assert 0 <= num_shots < len(word_list), "Number of shots must be between 0 and the number of words in the list, minus the chosen word."
+    word_list = [item for item in word_list if item[0] != word] # Remove any words that are the same as the word we want to spell.
     prompt = ''
-    if word in word_list:
-        word_list.remove(word) # Ensure we don't sample the same word we want to spell.
     if num_shots > 0:
         samples = random.sample(word_list, num_shots)
         for sample in samples:
             position = random.randint(0, len(sample[0])-1)
-            prompt += f"Q: What is the {INDEX_TO_POSITION[position]} letter of '{sample[0]}'? A: {sample[1][position]}\n\n"
+            prompt += f"Q: What is the {INDEX_TO_POSITION[position]} letter of '{sample[0]}'? A: {sample[1][position].upper()}\n\n"
     position = random.randint(0, len(word[0])-1)
     prompt += f"Q: What is the {INDEX_TO_POSITION[position]} letter of '{word}'? A:"
-    return prompt, word[position]
+    return prompt, word[position].upper()
 
 
 def format_position_of_letter_response(item):
@@ -178,20 +197,3 @@ def format_position_of_letter_response(item):
 def get_position_of_letter_accuracy(data: Dict[int, List[SpellingEvalDict]]) -> Dict[int, float]:
     """Takes in a dictionary of results, and returns the accuracy of the model on each grade."""
     return {grade: sum([1 if d['response'].startswith(d['answer']) else 0 for d in data[grade]]) / len(data[grade]) for grade in data.keys()}
-
-
-def get_spelling_accuracy(data: Dict[int, List[SpellingEvalDict]]) -> Dict[int, float]:
-    """Takes in a dictionary of results, and returns the accuracy of the model on each grade."""
-    return {grade: sum([1 if d['response'].startswith(d['answer']) else 0 for d in data[grade]]) / len(data[grade]) for grade in data.keys()}
-
-
-def run_multiple_shots(shots: List[int], eval: SpellingEval, 
-                       model, model_type: ModelType, tokenizer, 
-                       word_list: List[Tuple[str, str]],
-                       response_type: EvalResponse,
-                       batch_size: int=10) -> Dict[int, List[SpellingEvalDict]] | Dict[int, float]:
-    """Run an eval for multiple few-shot parameters."""
-    shots = {shot: {} for shot in shots}
-    for shot in shots:
-        shots[shot] = eval.run_eval(model, model_type, tokenizer, response_type, word_list, shot, batch_size)
-    return shots
